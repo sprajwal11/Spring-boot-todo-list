9935cc784dbf86b00c79511ba8aa19126a28a0ee
Prajwal <sharmaprajwal11@gmail.com>
2023-10-28 23:48:22 +0530

    first commit

---
+++ src/test/java/com/todolist/myfirstwebapp/MyfirstwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolist.myfirstwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
